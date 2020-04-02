a744e1434df1e10145200b7623dbd1cc560c2c7b
Remi ARLEN <rarlen@etu.unistra.fr>
2020-04-02 15:09:41 +0200

    résolution problème zones interdites null

--- map/components/map/Polygons.js
+++ map/components/map/Polygons.js
@@ -3,19 +3,22 @@ import {Polygon} from 'react-native-maps';
 import {formatArea} from '../../utils/game';
 
 const Polygons = ({areas}) => {
-  return areas.map(area => {
-    return (
-      <Polygon
-        strokeColor={
-          area.forbidden ? 'rgba(255, 0, 0, 0.5)' : 'rgba(0, 255, 0, 0.3)'
-        }
-        coordinates={formatArea(area)}
-        fillColor={
-          area.forbidden ? 'rgba(255, 0, 0, 0.3)' : 'rgba(0, 255, 0, 0.1)'
-        }
-      />
-    );
-  });
+  return (
+    areas &&
+    areas.map(area => {
+      return (
+        <Polygon
+          strokeColor={
+            area.forbidden ? 'rgba(255, 0, 0, 0.5)' : 'rgba(0, 255, 0, 0.3)'
+          }
+          coordinates={formatArea(area)}
+          fillColor={
+            area.forbidden ? 'rgba(255, 0, 0, 0.3)' : 'rgba(0, 255, 0, 0.1)'
+          }
+        />
+      );
+    })
+  );
 };
 
 export default Polygons;
