268b1a44226cb83210032c1eb7c6ecafb24b6b7c
Remi ARLEN <rarlen@etu.unistra.fr>
2020-04-15 17:37:41 +0200

    ajout bottomnavbar

---
+++ map/components/Menu.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import BottomNavigation, {
+  FullTab,
+} from 'react-native-material-bottom-navigation';
+import {View} from 'react-native';
+import MyGames from './partie/MyGames';
+import Game from './partie/Game';
+import Profil from './autentification/Profil';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faUserCircle,
+  faSearch,
+  faGamepad,
+} from '@fortawesome/free-solid-svg-icons';
+import {stylesSigninSignup} from '../css/style';
+
+const BottomTab = ({tabActive = 'myGames'}) => {
+  const tabs = [
+    {
+      key: 'myGames',
+      icon: 'myGames',
+      label: 'Mes parties',
+      barColor: '#388E3C',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+    {
+      key: 'findGames',
+      icon: 'findGames',
+      label: 'Trouver une partie',
+      barColor: '#B71C1C',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+    {
+      key: 'profil',
+      icon: 'profil',
+      label: 'Profil',
+      barColor: '#E64A19',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+  ];
+
+  const [activeTab, setActiveTab] = useState(tabActive);
+
+  const renderIcon = icon => ({isActive}) => (
+    <FontAwesomeIcon
+      icon={
+        icon === 'myGames'
+          ? faGamepad
+          : icon === 'findGames'
+          ? faSearch
+          : icon === 'profil' && faUserCircle
+      }
+      style={stylesSigninSignup.submitButtonText}
+    />
+  );
+
+  const renderTab = ({tab, isActive}) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={renderIcon(tab.icon)}
+    />
+  );
+
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        {activeTab === 'myGames' ? (
+          <MyGames />
+        ) : activeTab === 'findGames' ? (
+          <Game />
+        ) : (
+          activeTab === 'profil' && <Profil />
+        )}
+      </View>
+      <BottomNavigation
+        activeTab={activeTab}
+        onTabPress={newTab => setActiveTab(newTab.key)}
+        renderTab={renderTab}
+        tabs={tabs}
+      />
+    </View>
+  );
+};
+
+export default BottomTab;
---
+++ map/components/autentification/Profil.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'native-base';
+import {stylesGame} from '../../css/style';
+
+const Profil = () => {
+  return (
+    <>
+      <View style={stylesGame.container}>
+        <Text> Mon profil</Text>
+      </View>
+    </>
+  );
+};
+
+export default Profil;
